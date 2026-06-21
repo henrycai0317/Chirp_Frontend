4e1463942f83f4ed154285dd4910f8b6891fbb0c
Henry Cai <alaba.havai@gmail.com>
2026-06-21 12:35:23 +0800

    Add ChirpTheme and color scheme for consistent theming across the app

--- composeApp/src/commonMain/kotlin/com/plcoding/chirp/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/chirp/App.kt
@@ -17,10 +17,11 @@ import org.jetbrains.compose.resources.painterResource
 
 import chirp.composeapp.generated.resources.Res
 import chirp.composeapp.generated.resources.compose_multiplatform
+import com.plcoding.core.designsystem.theme.ChirpTheme
 
 @Composable
 fun App() {
-    MaterialTheme {
+    ChirpTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
